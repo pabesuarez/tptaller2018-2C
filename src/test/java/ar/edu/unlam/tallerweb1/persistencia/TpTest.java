b21a6f8896cb0e90d09869844f1e3f051528c9bd
DiegoLed <diegoledesma23@gmail.com>
2018-10-02 16:11:20 -0300

    Add files via upload

--- src/test/java/ar/edu/unlam/tallerweb1/persistencia/TpTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/persistencia/TpTest.java
@@ -42,7 +42,58 @@ public class TpTest extends SpringTest{
     }
     
     //TODO: buscar todos los paises de europa
-    
+    // Punto 3
+ 	@Test
+ 	@Transactional
+ 	@Rollback
+ 	public void buscarTodosPaisesEuropeos(){
+ 		
+ 		Pais elPais1 = new Pais();
+ 		elPais1.setNombrePais("Alemania");
+ 				
+ 		Pais elPais2 = new Pais();
+ 		elPais2.setNombrePais("EEUU");
+ 				
+ 		Pais elPais3 = new Pais();
+ 		elPais3.setNombrePais("Argentina");
+ 				
+ 		Pais elPais4 = new Pais();
+ 		elPais4.setNombrePais("Australia");
+ 		
+ 		Continente elContinente1 = new Continente();
+ 		elContinente1.setNombre("Europa");
+ 		Continente elContinente2 = new Continente();
+ 		elContinente2.setNombre("America");
+ 		Continente elContinente3 = new Continente();
+ 		elContinente3.setNombre("America");
+ 		Continente elContinente4 = new Continente();
+ 		elContinente4.setNombre("Oceania");
+ 		
+ 		//Asigno los continentes a los Paises
+ 		elPais1.setContinente(elContinente1);
+ 		elPais2.setContinente(elContinente2);
+ 		elPais3.setContinente(elContinente3);
+ 		elPais4.setContinente(elContinente4);
+ 		
+ 		//guardos los paises
+ 		getSession().save(elContinente1);
+ 		getSession().save(elContinente2);
+ 		getSession().save(elContinente3);
+ 		getSession().save(elContinente4);
+ 		
+ 		getSession().save(elPais1);
+ 		getSession().save(elPais2);
+ 		getSession().save(elPais3);
+ 		getSession().save(elPais4);
+ 		
+ 		List<Pais> paises = getSession().createCriteria(Pais.class)
+ 										.createAlias("continente","continenteBuscado")
+ 										.add(Restrictions.eq("continenteBuscado.nombre", "Europa"))
+ 										.list();
+ 		
+ 		assertThat(paises.size()).isEqualTo(1);
+ 		
+ 	}
     
     //buscar todos los paises con capital al norte del tropico de cancer (latitud>23.43722)
     @SuppressWarnings("unchecked")
@@ -83,5 +134,57 @@ public class TpTest extends SpringTest{
     }
     
     //TODO : buscar todos las ciudades del hemisferio sur (latitud<0)
-    
+	//Punto 5
+	@Test
+	@Transactional
+	@Rollback
+	public void buscarTodasCiudadesDelEsmiferioSur(){
+		
+		Ciudad ciudad1 = new Ciudad();
+		Ciudad ciudad2 = new Ciudad();
+		Ciudad ciudad3 = new Ciudad();
+		Ciudad ciudad4 = new Ciudad();
+		
+		ciudad1.setNombre("Berlin");
+		ciudad2.setNombre("Washigton");
+		ciudad3.setNombre("Buenos Aires");
+		ciudad4.setNombre("Camberra");
+		
+		Ubicacion ubicacion1 = new Ubicacion();
+		Ubicacion ubicacion2 = new Ubicacion();
+		Ubicacion ubicacion3 = new Ubicacion();
+		Ubicacion ubicacion4 = new Ubicacion();
+		
+		ubicacion1.setLatitud(30.00);
+		ubicacion2.setLatitud(28.00);
+		ubicacion3.setLatitud(-30.00);
+		ubicacion4.setLatitud(-28.00);
+		
+		getSession().save(ubicacion1);
+		getSession().save(ubicacion2);
+		getSession().save(ubicacion3);
+		getSession().save(ubicacion4);
+		
+		ciudad1.setUbicacion(ubicacion1);
+		ciudad2.setUbicacion(ubicacion2);
+		ciudad3.setUbicacion(ubicacion3);
+		ciudad4.setUbicacion(ubicacion4);
+		
+		getSession().save(ciudad1);
+		getSession().save(ciudad2);
+		getSession().save(ciudad3);
+		getSession().save(ciudad4);
+		
+		List<Ciudad> ciudades = getSession().createCriteria(Ciudad.class)
+											.createAlias("ubicacion", "ubicacionBuscada")
+											.add(Restrictions.le("ubicacionBuscada.latitud",00.00))
+											.list();
+		
+		assertThat(ciudades.size()).isEqualTo(2);
+		
+		for (Ciudad obj : ciudades) {
+			  System.out.println(obj.getNombre());
+			};
+		
+	}
 }
